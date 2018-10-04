a4f1077e21edeadb3a2ef925f48801f23fc05cf6
Rafael Pedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-10-04 13:57:55 -0300

    Initializing Reader

---
+++ src/trab/Reader.java
@@ -0,0 +1,14 @@
+package trab;
+
+import java.io.*;
+import java.util.*;
+
+public class Reader {
+
+    public static Set<Coalition> ReadFile() {
+
+        Locale brLocale = Locale.forLanguageTag("pt-Br");
+
+        Scaner input;
+    }
+}
