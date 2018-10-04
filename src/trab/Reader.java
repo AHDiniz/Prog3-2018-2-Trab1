f9adc1ae1f7028861ec77701182a50b7168a966d
Rafael Pedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-10-04 14:31:11 -0300

    Reader Update

--- src/trab/Reader.java
+++ src/trab/Reader.java
@@ -5,10 +5,33 @@ import java.util.*;
 
 public class Reader {
 
-    public static Set<Coalition> ReadFile() {
+    public static Set<Coalition> ReadFile(String filePath) {
+
+        Set<Coalition> coalitions = new HashSet<Coalition>();
 
         Locale brLocale = Locale.forLanguageTag("pt-Br");
 
-        Scaner input;
+        Scanner data = null;
+
+        try(Scanner file = new Scanner(new File(filePath));) {
+
+            file.useDelimiter(";");
+
+            while(file.hasNextLine()) {
+                data = new Scanner(file.nextLine());
+                while(data.hasNext()) {
+                    String aux = data.next()
+                    System.out.println(aux);
+                }
+                
+            }
+
+        }
+        catch(IOException ex) {
+            System.out.println("Erro de leitura");
+        }
+        
+
+        return coalitions;
     }
 }
