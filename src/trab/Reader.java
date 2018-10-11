a0a3a828515eac519e2fdc518a131071164a1b6c
Rafael Pedruzzi <rafaelbelmockpedruzzi@hotmail.com>
2018-10-11 16:38:25 -0300

    Reader coments

--- src/trab/Reader.java
+++ src/trab/Reader.java
@@ -7,64 +7,62 @@ public class Reader
 {
     public static Map<String,Coalition> readFile(String filePath, String encoding)
     {
-        Map<String,Coalition> coalitions = new HashMap<String,Coalition>();
-        int vacancies=0;
-        String aux, name, coalition, party, percent;
-        int votes=0;
-        Coalition temp;
+        Map<String,Coalition> coalitions = new HashMap<String,Coalition>(); // Map with all coalitions
+        int vacancies=0;                                                    // Total number of elected candidates
+        String aux, name, coalition, party, percent;                        // Auxiliar variables
+        int votes=0;                                                        // ...
+        Coalition temp;                                                     // ...
 
-        Locale brLocale = Locale.forLanguageTag("pt-Br");
-
-        Scanner data = null;
+        Locale brLocale = Locale.forLanguageTag("pt-Br"); // Seting locale
 
         try (Scanner file = new Scanner( new BufferedReader( new InputStreamReader(new FileInputStream(filePath), encoding)));)
         {
-        	file.useLocale(brLocale);
-        	file.nextLine();
+        	file.useLocale(brLocale);   // Seting file locale
+        	file.nextLine();            // Junping header
         	
-            while(file.hasNextLine())
+            while(file.hasNextLine())   // Reading every line of the entrance
             {
-                data = new Scanner(file.nextLine());
-                data.useLocale(brLocale);
-                data.useDelimiter(";");
+                Scanner line = new Scanner(file.nextLine()); // Seting a new scanner for the actual line
+                line.useLocale(brLocale); // Seting the locale in the line
+                line.useDelimiter(";"); // Seting line delimiter as ';' 
 
-                aux = data.next();
-                if(aux.startsWith("#"))
+                aux = line.next(); // Geting the identification number
+                if(aux.startsWith("#")) // Break the loop if the section of valid candidates end
                     {
                         break;
                     }
-                if(aux.startsWith("*"))
+                if(aux.startsWith("*")) // Incrementing the number of vacancies if find a elected candidate
                 {
                     vacancies++;
                 }
 
-                data.next();
-                name = data.next();
+                line.next(); // Junping the candidate's number
+                name = line.next(); // Geting the candidate's name
 
-                Scanner parties = new Scanner(data.next());
-                parties.useDelimiter("-");
-                party = (parties.next()).trim();
-                if(parties.hasNext())
+                Scanner parties = new Scanner(line.next()); // Seting a auxiliar scanner for the parties and coligations
+                parties.useDelimiter("-"); // Seting delimiter as '-'
+                party = (parties.next()).trim(); // Geting the candidate's party
+                if(parties.hasNext()) // If it have a coligation
                 {
-                    coalition = (parties.next()).trim();
+                    coalition = (parties.next()).trim(); // Geting the coligation
                 }
-                else
+                else // Else, seting it as the party
                 {
                     coalition = party;
                 }
-                parties.close();
+                parties.close(); // Closing the auxiliar scanner
 
-                votes = data.nextInt();
-                percent = data.next();
+                votes = line.nextInt(); // Geting the candidate's votes
+                percent = line.next(); // Geting the candidate's percent of votes
 
-                temp = coalitions.get(coalition);
-                if(temp == null)
+                temp = coalitions.get(coalition); // Serching the candidate's coalition
+                if(temp == null) // If there's none
                 {
-                    temp = new Coalition();
-                    coalitions.put(coalition, temp);
+                    temp = new Coalition(); // Creating a new coalition
+                    coalitions.put(coalition, temp); // And add it to the Map
                 }
 
-                temp.addCandidate(name, party, votes, percent);
+                temp.addCandidate(name, party, votes, percent); // Adding candidate to coalition
 
                 //System.out.println("\nName = "+name+"\nParty = ["+party+"]\nCoalition = ["+coalition+"]\nVotes = "+votes+"\nPercent = "+percent);
                 
